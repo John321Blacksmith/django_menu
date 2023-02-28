99936e25f0ed12746ffb52187ba554c23c07d328
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-02-28 18:26:13 +0300

    the orm has been part-ly implemented

--- tree_menu/models.py
+++ tree_menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+# define a blue print of the base menu object
+class BaseMenu(models.Model):
+	title = models.CharField(max_length=50, verbose_name='title')
+	slug = models.SlugField(max_length=255, verbose_name='slug', unique=True,
+							help_text='Use this snippet along with a template tag as a name of the rendered menu')
+	named_url = models.CharField(max_length=255)
+
+
+	def __str__(self):
+		return self.title
--- tree_menu/views.py
+++ tree_menu/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import BaseMenu
 # Create your views here.
 
-def index(request):
-	pass
-	
+
+class Categories:
+	model = BaseMenu
+	template_name = 'index.html'
+
+	def get(self, request, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+
+		context['categories'] = BaseMenu.objects.all.filter(existence=True)
+
+		return HttpResponse(context)
