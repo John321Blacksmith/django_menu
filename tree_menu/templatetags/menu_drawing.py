6c8b5fd7dd282ef1a72180bce5847cb3612f2999
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-03-02 20:10:30 +0300

    changed method in the module query

--- tree_menu/templatetags/menu_drawing.py
+++ tree_menu/templatetags/menu_drawing.py
@@ -10,8 +10,9 @@ def draw_menu(context, slug):
 	 # here I could use the 'select_related()' method but it's only
 	 # used in case of a single object, whereas the 'prfetch_selected()' one 
 	 # does the same stuff but with a whole set of things
-		main_points = Menu.objects.prefetch_related('items__items__items__items').get(slug=slug)
+		main_points = Menu.objects.all().filter(existence=True)
 
 		return {'main_points': main_points, 'context': context}
-	except Menu.DoesNotExist:
+	except Menu.DoesNotExist as err:
+		print(err)
 		return {'main_points': '', 'context': context}
