bb855106e358111d96b254966a1414bece215f65
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-03-03 12:48:19 +0300

    deleted an excessive comment

--- tree_menu/templatetags/menu_drawing.py
+++ tree_menu/templatetags/menu_drawing.py
@@ -7,9 +7,6 @@ register = template.Library()
 @register.inclusion_tag('tree_menu/template_markup/menu_blueprint.html', takes_context=True)
 def draw_menu(context, slug):
 	try:
-	 # here I could use the 'select_related()' method but it's only
-	 # used in case of a single object, whereas the 'prfetch_selected()' one 
-	 # does the same stuff but with a whole set of things
 		main_points = Menu.objects.all().filter(existence=True)
 
 		return {'main_points': main_points, 'context': context}
