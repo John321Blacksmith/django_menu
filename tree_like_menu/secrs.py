eb621d65f7dcd2e95706f3fc9815b7adb67d8e9f
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-03-06 14:01:27 +0300

    the last change

---
+++ tree_like_menu/secrs.py
@@ -0,0 +1,6 @@
+SECRET_PROD_KEY = 'you production secret key'
+PROJECT_LOCATION = 'your project location (optional)'
+
+# django admin panel secrets
+username = 'your superuser login'
+password = 'your superuser password'
